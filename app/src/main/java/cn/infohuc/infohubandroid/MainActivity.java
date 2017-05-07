1fe26be90b0886c36937838f831678c5947ed636
toctrash <1124850791@qq.com>
2017-05-07 16:50:01 +0800

    hiing

--- app/src/main/java/cn/infohuc/infohubandroid/MainActivity.java
+++ app/src/main/java/cn/infohuc/infohubandroid/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView myTextView;
+    private EditText myEditView;
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_STRING_USER = "user";
     @Override
@@ -16,6 +20,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate");
 
+        myTextView = (TextView) findViewById(R.id.myTextView);
+        myEditView = (EditText) findViewById(R.id.myEditText);
+        final Button sayHi = (Button) findViewById(R.id.sayHi);
+
+        sayHi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sayHi(v);
+            }
+        });
+    }
+
+    public void sayHi(View v){
+        String buff = myEditView.getText().toString();
+        myTextView.setText(String.format(getString(R.string.hiing), buff));
     }
     public void sayHello(View v){
         Log.d(TAG, "ssyHello");
