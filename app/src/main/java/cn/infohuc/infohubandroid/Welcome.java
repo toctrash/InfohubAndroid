f5187e717f60d0a18bbd9ba9874585d81b8c9e2c
toctrash <1124850791@qq.com>
2017-05-14 16:04:31 +0800

    Responding to clicks

--- app/src/main/java/cn/infohuc/infohubandroid/Welcome.java
+++ app/src/main/java/cn/infohuc/infohubandroid/Welcome.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.List;
+
 
 public class Welcome extends AppCompatActivity {
     private static final String TAG = Welcome.class.getSimpleName();
@@ -35,11 +38,19 @@ public class Welcome extends AppCompatActivity {
             adapter.insertName(name);
         }
 
-        List<String> names = adapter.getAllNames();
+        final List<String> names = adapter.getAllNames();
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
         ListView listView = (ListView) findViewById(R.id.list_view);
 
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "item at " + position + " clicked ");
+                greetingText.setText(String.format(getString(R.string.greeting), names.get(position)));
+            }
+        });
 
     }
 }
